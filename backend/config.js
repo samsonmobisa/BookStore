1713ef4cee2a432cfe7b2bbbb3f409971945e33c
samsonmobisa <samsonmobisa62@gmail.com>
2024-01-09 16:43:07 +0300

    my first react app using vite

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+
+'mongodb+srv://sammobisa:<sam14535463>@bookstore.ekxoxsz.mongodb.net/BookStore?retryWrites=true&w=majority'
+
+  // 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
